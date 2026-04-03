f1eb53f6a2cf9dfb60a9eeb8150d45f463c4bf7b
El Chappell <e.chappell1@lancaster.ac.uk>
2026-04-03 16:49:19 +0100

    Added basic html and js to add and save ingredients

---
+++ script.js
@@ -0,0 +1,50 @@
+const addButton = document.getElementById('addIngredient');
+const ingredientInput = document.getElementById('ingredientName');
+const ingredientList = document.getElementById('ingredientList');
+
+loadIngredients();
+
+function addIngredient(){
+    const ingredient = ingredientInput.value;
+    if (ingredient){
+        createIngredElement(ingredient);
+        ingredientInput.value = '';
+
+        saveIngredient();
+    }
+    else {
+        alert('Please enter an ingredient')
+    }
+
+}
+
+addButton.addEventListener('click', addIngredient);
+
+function createIngredElement (ingredient){
+    const listItem  = document.createElement('li');
+    listItem.textContent = ingredient;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    listItem.appendChild(deleteButton);
+
+    deleteButton.addEventListener('click', function(){
+        taskList.removeChild(listItem);
+    })
+
+    ingredientList.appendChild(listItem);
+}
+
+function saveIngredient(){
+    let ingredients = [];
+    ingredientList.querySelectorAll('li').forEach(function(item){
+        ingredients.push(item.textContent);
+    });
+
+    localStorage.setItem('ingredients', JSON.stringify(ingredients));
+}
+
+function loadIngredients(){
+    const ingredients = JSON.parse(localStorage.getItem('ingredients')) || [];
+    ingredients.forEach(createIngredElement);
+}
