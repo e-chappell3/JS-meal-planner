57520c09eb36d983cb488b042684684fb928cf9d
El Chappell <e.chappell1@lancaster.ac.uk>
2026-04-03 18:33:06 +0100

    Updated script.js to include ingredient.java and recipe.java object functionality.

--- script.js
+++ script.js
@@ -1,8 +1,56 @@
+function Ingredient(name, price, isOwned){
+    this.name = name;
+    this.price = price;
+    this.isOwned = isOwned;
+}
+
+function Recipe(ingreds){
+    this.ingreds = ingreds;
+    let cost = 0;
+    let totalCost = 0 + ingreds.forEach(sum);
+    let required = setMake(required);
+    cost = 0;
+    let reqCost = 0 + required.forEach(sum);
+    if (required.length == 0){
+        let canMake = true;
+    }
+    else{
+        let canMake = false;
+    }
+}
+
+function sum(num){
+    cost += num;
+}
+
+function setMake(required){
+    for (let i = 0; i < ingreds.length(); i++){
+        let curr = ingreds[i];
+        if (curr.isOwned == false){
+            if (canMake != false){
+                canMake = false;
+            }
+            if(required.includes(curr) == false){
+                required.push(curr);
+            }
+        }
+        else if (required.includes(curr)){
+            const index = required.indexOf(curr);
+            if (index > -1){
+                required.splice(index, 1);
+            }
+        }
+    }
+
+    return required;
+}
+
 const addButton = document.getElementById('addIngredient');
 const ingredientInput = document.getElementById('ingredientName');
 const ingredientList = document.getElementById('ingredientList');
 
 loadIngredients();
+addButton.addEventListener('click', addIngredient);
 
 function addIngredient(){
     const ingredient = ingredientInput.value;
@@ -17,8 +65,6 @@ function addIngredient(){
     }
 
 }
-
-addButton.addEventListener('click', addIngredient);
 
 function createIngredElement (ingredient){
     const listItem  = document.createElement('li');
